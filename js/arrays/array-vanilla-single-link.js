5e3b48dd4b235d4b3b20fe4f0c59aa021ca1c01f
Navas <gabrielnavas@email.com>
2021-02-13 18:58:55 -0300

    refactoy push and unshift of the ArraySingleLink

--- js/arrays/array-vanilla-single-link.js
+++ js/arrays/array-vanilla-single-link.js
@@ -1,153 +1,161 @@
 /**
  * INITITAL_NODE -> NODE -> NODE -> NODE -\\
  */
+class Node {
+  constructor(data = null, next = null) {
+    this.data = data
+    this.next = next
+  }
+}
 
-const makeNewNode = (data = 0, nextNode = null) => ({ 
-  nextNode, 
-  data
-}) 
-
-class ArraySingleNode {
+class ArraySingleLink {
   constructor( ) {
-    this.initialNode = null
+    this.head = null
+    this.count = 0
   }
 
+  push = data => {
+    if(this.head === null) {
+      this.head = new Node(data)
+    } else {
+      let previousNode = this.head
+      let nodeNow = previousNode.next
+      while(nodeNow !== null) {
+        previousNode = nodeNow
+        nodeNow = nodeNow.next
+      }
+      previousNode.next = new Node(data)
+    }
+    return ++this.count
+  }
+
+  unshift = data => {
+    if(this.head === null) {
+      this.head = new Node(data)
+    } else {
+      const newNode = new Node(data, this.head)
+      this.head = newNode
+    }
+    return ++this.count
+  }
+
+  // isEmpty = () => this.head === null
+
+  // pop = () => {
+  //   if(this.head.nextNode === null) {
+  //     this.head = null
+  //     return this
+  //   }
+  //   let previousNode = this.head
+  //   let nodeNow = previousNode.nextNode
+  //   while(nodeNow.nextNode !== null) {
+  //     previousNode = nodeNow
+  //     nodeNow = nodeNow.nextNode
+  //   }
+  //   previousNode.nextNode = null
+  //   return this
+  // }
+
+  // shift = () => {
+  //   this.head = this.head.nextNode
+  //   return this
+  // }
+
+  // reverse = () => {
+  //   const reverseRecursive = (nodeNow) => {
+  //     if(nodeNow.nextNode !== null) {
+  //       const nextNode = reverseRecursive(nodeNow.nextNode)
+  //       if(nextNode.nextNode === null) {
+  //         this.head = nodeNow
+  //         return nodeNow
+  //       }
+  //       nextNode.nextNode = nodeNow
+  //     } 
+  //     return nodeNow
+  //   }
+  //   const lastNode = reverseRecursive(this.head)
+  //   lastNode.nextNode = null
+  //   return this
+  // }
+
+  // join = (separator=',') => {
+  //   let str = ''
+  //   let node = this.head
+  //   while(node !== null) {
+  //     str += node.nextNode ? `${node.data}${separator}` : `${node.data}`
+  //     node = node.nextNode
+  //   }
+  //   return str
+  // }
+
+  // indexOf = (data) => {
+  //   let position = 0
+  //   let node = this.head
+  //   while(node !== null && node.data !== data) {
+  //     position = position + 1
+  //     node = node.nextNode
+  //   }
+  //   return node === null ? -1 : position
+  // }
+
+  // lastIndexOf = (data) => {
+  //   let position = 0
+  //   let lastPosition = -1
+  //   let node = this.head
+  //   while(node !== null) {
+  //     if(node.data === data) {
+  //       lastPosition = position
+  //     }
+  //     position = position + 1
+  //     node = node.nextNode
+  //   }
+  //   return lastPosition
+  // }
+
+  // forEach = callback => {
+  //   let index = 0
+  //   let inititalNodeReference = this.head
+  //   let node = this.head
+  //   while(node !== null) {
+  //     callback(index, node.data, inititalNodeReference)
+  //     node = node.nextNode
+  //     index = index+1
+  //   }
+  //   return this
+  // }
+
+  // concatOtherArray = (otherArray) => {
+  //   if(this.head === null) {
+  //     this.head = otherArray
+  //     return this
+  //   }
+  //   let node = this.head
+  //   while(node.nextNode !== null) {
+  //     node = node.nextNode
+  //   }  
+  //   node.nextNode = otherArray
+  // }
+
   toString = () => {
-    let node = this.initialNode
+    let node = this.head
     let str = '[ '
     while(node !== null) {
-      if(node.nextNode === null) {
+      if(node.next === null) {
         str += `${node.data} `
       } else {
         str += `${node.data}, `
       }
-      node = node.nextNode
+      node = node.next
     }
     str += ']'
     return str
   }
-
-  push = data => {
-    if(this.initialNode === null) {
-      this.initialNode = makeNewNode(data)
-      return this
-    }
-    let previousnode = this.initialNode
-    let nodeNow = previousnode.nextNode
-    while(nodeNow !== null) {
-      previousnode = nodeNow
-      nodeNow = nodeNow.nextNode
-    }
-    previousnode.nextNode = makeNewNode(data)
-    return this
-  }
-
-  unshift = data => {
-    if(this.initialNode === null) {
-      this.initialNode = makeNewNode(data)
-      return this
-    }
-    const oldFirstNode = this.initialNode
-    const newFirstNode = makeNewNode(data)
-    this.initialNode = newFirstNode
-    newFirstNode.nextNode = oldFirstNode
-    return this
-  }
-
-  isEmpty = () => this.initialNode === null
-
-  pop = () => {
-    if(this.initialNode.nextNode === null) {
-      this.initialNode = null
-      return this
-    }
-    let previousNode = this.initialNode
-    let nodeNow = previousNode.nextNode
-    while(nodeNow.nextNode !== null) {
-      previousNode = nodeNow
-      nodeNow = nodeNow.nextNode
-    }
-    previousNode.nextNode = null
-    return this
-  }
-
-  shift = () => {
-    this.initialNode = this.initialNode.nextNode
-    return this
-  }
-
-  reverse = () => {
-    const reverseRecursive = (nodeNow) => {
-      if(nodeNow.nextNode !== null) {
-        const nextNode = reverseRecursive(nodeNow.nextNode)
-        if(nextNode.nextNode === null) {
-          this.initialNode = nodeNow
-          return nodeNow
-        }
-        nextNode.nextNode = nodeNow
-      } 
-      return nodeNow
-    }
-    const lastNode = reverseRecursive(this.initialNode)
-    lastNode.nextNode = null
-    return this
-  }
-
-  join = (separator=',') => {
-    let str = ''
-    let node = this.initialNode
-    while(node !== null) {
-      str += node.nextNode ? `${node.data}${separator}` : `${node.data}`
-      node = node.nextNode
-    }
-    return str
-  }
-
-  indexOf = (data) => {
-    let position = 0
-    let node = this.initialNode
-    while(node !== null && node.data !== data) {
-      position = position + 1
-      node = node.nextNode
-    }
-    return node === null ? -1 : position
-  }
-
-  lastIndexOf = (data) => {
-    let position = 0
-    let lastPosition = -1
-    let node = this.initialNode
-    while(node !== null) {
-      if(node.data === data) {
-        lastPosition = position
-      }
-      position = position + 1
-      node = node.nextNode
-    }
-    return lastPosition
-  }
-
-  forEach = callback => {
-    let index = 0
-    let inititalNodeReference = this.initialNode
-    let node = this.initialNode
-    while(node !== null) {
-      callback(index, node.data, inititalNodeReference)
-      node = node.nextNode
-      index = index+1
-    }
-    return this
-  }
-
-  concatOtherArray = (otherArray) => {
-    if(this.initialNode === null) {
-      this.initialNode = otherArray
-      return this
-    }
-    let node = this.initialNode
-    while(node.nextNode !== null) {
-      node = node.nextNode
-    }  
-    node.nextNode = otherArray
-  }
 }
+
+const arr = new ArraySingleLink
+console.log(arr.push(3))
+console.log(arr.push(5))
+console.log(arr.unshift(7))
+console.log(arr.unshift(11))
+console.log(arr.unshift(15))
+console.log(arr.toString())
